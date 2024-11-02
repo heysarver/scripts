75066f9a813178239c54263a9b2bd54e20b9e275
heysarver <22250203+heysarver@users.noreply.github.com>
2024-11-02 00:26:16 -0400

    install fluentd

---
+++ bash/install_fluentd/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## this defaults to redhat distros but i want it to default to ubuntu
+## let the user specify lts or not, default to not
+
+# or curl -fsSL https://toolbelt.treasuredata.com/sh/install-redhat-fluent-package5-lts.sh | sh
+curl -fsSL https://toolbelt.treasuredata.com/sh/install-redhat-fluent-package5.sh | sh
